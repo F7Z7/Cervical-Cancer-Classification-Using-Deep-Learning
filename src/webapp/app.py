3360bac9cb1fb304ecda10193effe179fb1c67ed
farza <farzanrichu610@gmail.com>
2025-10-22 15:06:32 +0530

    initial set up

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def hello():
     return "<p>Hello, World!</p>"
 
-
+@app.route('/hello/<name>')
+def welcome(name):
+    return 'hello %s' %name
 
 if __name__ == '__main__':
     app.run(debug=True)
