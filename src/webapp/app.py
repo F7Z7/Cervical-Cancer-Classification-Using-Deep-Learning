065d662c3a5b5052688a4537490d88fae1f85294
farza <farzanrichu610@gmail.com>
2025-10-22 19:24:04 +0530

    full set

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -4,30 +4,43 @@ import os
 
 from src.predictor.image_predictor import ImagePredictor
 
-app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'static'))
 predictor = ImagePredictor()
 
 
 @app.route('/',methods=['GET','POST'])
 def predict():
+    print("Route '/' accessed")
     if request.method == 'POST':
         if "image" not in request.files or request.files["image"].filename == "":
             return render_template("upload.html", error="Please select a file.")
 
+        print("ivde ethi")
         file=request.files["image"]
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
 
+        print("model predict cheyth thudangi")
+        import time
 
-        label,prob = predictor.predict(file_path)
+        print("Model prediction starting")
+        start_time = time.time()
+        label, prob = predictor.predict(file_path)
+        end_time = time.time()
+        print(f"Model prediction done in {end_time - start_time:.2f} seconds")
+
         prob_text = f"Abnormal: {prob[0] * 100:.2f}% | Normal: {prob[1] * 100:.2f}%"
 
-        image_url = url_for('static', filename=f'uploads/{file.filename}')
 
-        return render_template("predictor.html",prediction=label,probability=prob_text)
+        image_url = url_for('static', filename=f'uploads/{file.filename}')
+        print(image_url)
+
+        print("done")
+        return render_template("predictor.html",prediction=label,probability=prob_text,image_path=image_url)
 
     return render_template("predictor.html")
 
