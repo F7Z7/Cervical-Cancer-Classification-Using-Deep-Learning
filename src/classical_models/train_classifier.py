a8b6e95a06c8f6d56e3cd351c1ee4a49049b78f2
farza <farzanrichu610@gmail.com>
2025-10-23 22:55:06 +0530

    formatted code

--- src/classical_models/train_classifier.py
+++ src/classical_models/train_classifier.py
@@ -6,21 +6,19 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+train_features = np.load(
+    'C:\\Users\\farza\\PycharmProjects\\Cervical-Cancer-Classification-Using-Deep-Learning\\data\\features\\resnet152_train_features.npz')
+test_features = np.load(
+    'C:\\Users\\farza\\PycharmProjects\\Cervical-Cancer-Classification-Using-Deep-Learning\\data\\features\\resnet152_test_features.npz')
 
-
-train_features=np.load('C:\\Users\\farza\\PycharmProjects\\Cervical-Cancer-Classification-Using-Deep-Learning\\data\\features\\resnet152_train_features.npz')
-test_features=np.load('C:\\Users\\farza\\PycharmProjects\\Cervical-Cancer-Classification-Using-Deep-Learning\\data\\features\\resnet152_test_features.npz')
-
-X_train,y_train=train_features['X'],train_features['y']
-X_test,y_test=test_features['X'],test_features['y']
+X_train, y_train = train_features['X'], train_features['y']
+X_test, y_test = test_features['X'], test_features['y']
 
 print(f"Train shape: {X_train.shape}, Test shape: {X_test.shape}")
 
-
-scalar=StandardScaler()
+scalar = StandardScaler()
 X_train_scaled = scalar.fit_transform(X_train)
-X_test_scaled  = scalar.transform(X_test)
-
+X_test_scaled = scalar.transform(X_test)
 
 logistic_model = LogisticRegression(
     max_iter=3000,
@@ -30,8 +28,6 @@ logistic_model = LogisticRegression(
 )
 logistic_model.fit(X_train_scaled, y_train)
 
-
-
 y_pred = logistic_model.predict(X_test_scaled)
 
 print(classification_report(y_test, y_pred))
@@ -40,22 +36,18 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
 cm = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(5,4))
+plt.figure(figsize=(5, 4))
 sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
-            xticklabels=["Abnormal","Normal"],
-            yticklabels=["Abnormal","Normal"])
+            xticklabels=["Abnormal", "Normal"],
+            yticklabels=["Abnormal", "Normal"])
 plt.xlabel("Predicted")
 plt.ylabel("True")
 plt.title("Confusion Matrix - Logistic Regression")
 plt.tight_layout()
 plt.show()
 
-
-
-
 import os
 
 os.makedirs("models", exist_ok=True)
 joblib.dump(logistic_model, "models/logistic_model.joblib")
 joblib.dump(scalar, "models/scalar.joblib")
-
